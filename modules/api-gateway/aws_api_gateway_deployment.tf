a6e6f51a535c7bbe117ecfac16baceefb744272e
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-10-25 15:51:33 +0900

    足りないリソース

---
+++ modules/api-gateway/aws_api_gateway_deployment.tf
@@ -0,0 +1,11 @@
+resource "aws_api_gateway_deployment" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.lambda.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.lambda.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
