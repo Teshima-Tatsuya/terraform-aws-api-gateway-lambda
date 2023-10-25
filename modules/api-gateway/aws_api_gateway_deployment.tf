41813aa5092cc99ac74d1153cdbfaaca3518d972
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-10-25 16:00:23 +0900

    add stage_name

--- modules/api-gateway/aws_api_gateway_deployment.tf
+++ modules/api-gateway/aws_api_gateway_deployment.tf
@@ -1,5 +1,6 @@
 resource "aws_api_gateway_deployment" "lambda" {
   rest_api_id = aws_api_gateway_rest_api.lambda.id
+  stage_name = aws_api_gateway_stage.stage_name
 
   triggers = {
     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.lambda.body))
