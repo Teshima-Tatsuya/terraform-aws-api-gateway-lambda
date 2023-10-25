c4c1ad784610aba2f8d76496b5e8bb02180dfa0b
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-10-25 15:32:58 +0900

    fix lambda_handler name

--- modules/lambda/aws_lambda_function.tf
+++ modules/lambda/aws_lambda_function.tf
@@ -2,6 +2,6 @@ resource "aws_lambda_function" "helloworld" {
   function_name = "api-gateway-sample"
   role          = var.role
   handler       = "helloworld.lambda_handler"
-  runtime       = "python3.10"
+  runtime       = "python3.8"
   filename      = "${path.module}/archive/helloworld.zip"
 }
