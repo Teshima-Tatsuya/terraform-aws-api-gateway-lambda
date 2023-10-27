0e2750bb73d9c862722817375004463675afe041
Tatsuya Teshima <terima666tatsuya@gmail.com>
2023-10-27 17:03:05 +0900

    add domain_name

--- modules/api-gateway/aws_api_gateway_domain_name.tf
+++ modules/api-gateway/aws_api_gateway_domain_name.tf
@@ -5,4 +5,8 @@ data "aws_acm_certificate" "aws" {
 resource "aws_api_gateway_domain_name" "api" {
   certificate_arn = data.aws_acm_certificate.aws.arn
   domain_name = "api.aws.tessy.dev"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
